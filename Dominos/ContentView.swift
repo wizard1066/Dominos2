af793faf601f1b1b7c0e83c29da7cc42e1fddc1a
Mark Lucking <mark.lucking@gmail.com>
2020-04-09 19:49:13 +0200

    fixing binding WIP

--- Dominos/ContentView.swift
+++ Dominos/ContentView.swift
@@ -7,20 +7,37 @@
 //
 
 import SwiftUI
+import Combine
 
 enum DragState {
   case inactive
   case dragging(translation: CGSize)
 }
 
+struct newView:Identifiable {
+  var id:UUID? = UUID()
+  var highImage:String = ""
+  var lowImage:String = ""
+}
+
+class newViews: ObservableObject {
+  var didChange = PassthroughSubject<Void, Never>()
+
+  @Published var nouViews: [newView]
+  
+  init() {
+    self.nouViews = allocateImages()
+  }
+}
+
 struct Fonts {
-    static func zapfino (size:CGFloat) -> Font{
-        return Font.custom("Zapfino",size: size)
-    }
+  static func zapfino (size:CGFloat) -> Font{
+    return Font.custom("Zapfino",size: size)
+  }
 }
 
 struct ContentView: View {
-  @State var magnificationValue: CGFloat = CGFloat(1)
+  @ObservedObject var novelleViews = newViews()
   
   var body: some View {
     let screenSize = UIScreen.main.bounds
@@ -30,73 +47,80 @@ struct ContentView: View {
       ScrollView(Axis.Set.horizontal, showsIndicators: true) {
         VStack {
           ZStack {
-          Rectangle()
-            .fill(Color.yellow)
-            .frame(minWidth: screenWidth * 4, maxHeight: screenHeight * 0.6)
-//            .gesture(MagnificationGesture()
-//            .onChanged { value in
-//            self.magnificationValue = value
-//            })
-//            .scaleEffect(magnificationValue)
-          Text("Dominos with Better Programming")
-            .font(Fonts.zapfino(size: 128))
-            .opacity(0.2)
-          }.onAppear {
-            allocateImages()
+            Rectangle()
+              .fill(Color.yellow)
+              .frame(minWidth: screenWidth * 4, maxHeight: screenHeight * 0.6)
+            //            .gesture(MagnificationGesture()
+            //            .onChanged { value in
+            //            self.magnificationValue = value
+            //            })
+            //            .scaleEffect(magnificationValue)
+            Text("Dominos with Better Programming")
+              .font(Fonts.zapfino(size: 128))
+              .opacity(0.2)
+          }.onTapGesture {
+            
+//            let foo = newView(id: nil, highImage: "image_part_011:image_part_011", lowImage: "image_part_011:image_part_012")
+//            self.novelleViews.nouViews[0] = foo
+            
+            print("fuck ",self.novelleViews.nouViews)
           }
         }
         HStack {
-          ForEach((0 ..< 7), id: \.self) { column in
-            DominoWrapper(highImage: "image_part_011", lowImage: "image_part_011", spin: 0)
+          ForEach((0 ..< 5), id: \.self) { column in
+            DominoWrapper(novelleViews: self.novelleViews, column: column, spin: 0)
           }
         }
       }
       
-    }
+      }
+    
   }
 }
 
 struct DominoWrapper: View {
-  @State var highImage:String
-  @State var lowImage:String
+  @ObservedObject var novelleViews:newViews
+//  @State var highImage:String = ""
+//  @State var lowImage:String = ""
+  var column:Int
   @State var spin:Double
   @State var xpin:Double = -180
   @State var dragOffset = CGSize.zero
   @State var accumulated = CGSize.zero
   @State var rotateAngle:Double = 0
   @State var hideBack = false
-
   
   var body: some View {
-    ZStack {
+   
+    return Group { ZStack {
       Back(spin: -spin).onTapGesture {
-      withAnimation(Animation.easeInOut(duration: 1.5).delay(0)) {
-        self.spin = 180
-      }
-    }.rotation3DEffect(.degrees(spin), axis: (x: 0, y: 1, z: 0))
-    .opacity(hideBack ? 0:1)
-    if self.spin == 180 {
-      Domino(highImage: highImage, lowImage: lowImage, spin: xpin).onAppear {
         withAnimation(Animation.easeInOut(duration: 1.5).delay(0)) {
-          self.xpin = 0
-          self.hideBack = true
+          self.spin = 180
         }
-      }.rotation3DEffect(.degrees(xpin), axis: (x: 0, y: 1, z: 0))
-        .gesture(LongPressGesture()
-          .onEnded({ (_) in
-            withAnimation {
-              if self.rotateAngle < 360 {
-                self.rotateAngle += 90
-              } else {
-                self.rotateAngle = 0
+      }.rotation3DEffect(.degrees(spin), axis: (x: 0, y: 1, z: 0))
+        .opacity(hideBack ? 0:1)
+      if self.spin == 180 {
+        Domino(spin: $xpin, novelleViews: novelleViews).onAppear {
+          withAnimation(Animation.easeInOut(duration: 1.5).delay(0)) {
+            self.xpin = 0
+            self.hideBack = true
+          }
+        }.rotation3DEffect(.degrees(xpin), axis: (x: 0, y: 1, z: 0))
+          .gesture(LongPressGesture()
+            .onEnded({ (_) in
+              withAnimation {
+                if self.rotateAngle < 360 {
+                  self.rotateAngle += 90
+                } else {
+                  self.rotateAngle = 0
+                }
+                print("rotateAngle ",self.rotateAngle)
               }
-              print("rotateAngle ",self.rotateAngle)
-            }
-            }
-          )
-      ).rotationEffect(.degrees(self.rotateAngle), anchor: .center)
-    }
-    
+              }
+            )
+        ).rotationEffect(.degrees(self.rotateAngle), anchor: .center)
+      }
+      
     }.offset(x: self.dragOffset.width, y: self.dragOffset.height)
       .gesture(DragGesture(coordinateSpace: .global)
         .onChanged({ ( value ) in
@@ -108,14 +132,13 @@ struct DominoWrapper: View {
           self.accumulated = self.dragOffset
         }
     )
-    
+    }
   }
 }
 
 struct Domino: View {
-  @State var highImage:String
-  @State var lowImage:String
-  @State var spin:Double
+  @Binding var spin:Double
+  @ObservedObject var novelleViews:newViews
   
   var body: some View {
     Rectangle()
@@ -123,19 +146,20 @@ struct Domino: View {
       .frame(width: 48, height: 96, alignment: .center)
       .background(
         VStack{
-          Image(highImage)
+          Image(self.novelleViews.nouViews[1].highImage)
             .resizable()
             .frame(width: 32, height: 32, alignment: .top)
             .padding(4)
           Divider()
             .frame(width: 24, height: 2, alignment: .center)
-          Image(lowImage)
+          Image(self.novelleViews.nouViews[1].lowImage)
             .resizable()
             .frame(width: 32, height: 32, alignment: .bottom)
             .padding(4)
         }
     ).overlay(RoundedRectangle(cornerRadius: 8)
-          .stroke(lineWidth: 2))
+      .stroke(lineWidth: 2))
+      
   }
 }
 
@@ -160,29 +184,29 @@ struct Back: View {
             .padding(4)
         }
     ).overlay(RoundedRectangle(cornerRadius: 8)
-          .stroke(lineWidth: 2))
-          
+      .stroke(lineWidth: 2))
+    
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    ContentView(novelleViews: newViews.init())
   }
 }
 
-func allocateImages() {
+func allocateImages() -> [newView] {
   var primaryImages:Set<String> = ["image_part_002","image_part_003","image_part_004","image_part_005","image_part_006"]
   var secondaryImages:Set<String> = []
   var tiles:Set<String> = []
   
-    let elementA = primaryImages.randomElement()
-    primaryImages.remove(elementA!)
-    let elementB = primaryImages.randomElement()
-    primaryImages.remove(elementB!)
-    tiles.insert(elementA! + ":" + elementB!)
-    secondaryImages.insert(elementA!)
-    secondaryImages.insert(elementB!)
+  let elementA = primaryImages.randomElement()
+  primaryImages.remove(elementA!)
+  let elementB = primaryImages.randomElement()
+  primaryImages.remove(elementB!)
+  tiles.insert(elementA! + ":" + elementB!)
+  secondaryImages.insert(elementA!)
+  secondaryImages.insert(elementB!)
   repeat {
     let elementC = primaryImages.randomElement()
     primaryImages.remove(elementC!)
@@ -191,9 +215,23 @@ func allocateImages() {
     secondaryImages.remove(elementD!)
     tiles.insert(elementC! + ":" + elementD!)
   } while !primaryImages.isEmpty
-    let elementE = secondaryImages.removeFirst()
-    let elementF = secondaryImages.randomElement()
-    tiles.insert(elementE + ":" + elementF!)
+  let elementE = secondaryImages.removeFirst()
+  let elementF = secondaryImages.randomElement()
+  tiles.insert(elementE + ":" + elementF!)
   print("tiles ",tiles,secondaryImages.count)
+  
+  var answer:[newView] = []
+  for tile in tiles {
+    var highImage:String!
+    var lowImage:String!
+    let images = tile.split(separator: ":").map(String.init)
+      if images != [] {
+        highImage = images[0]
+        lowImage = images[1]
+      }
+    let foo = newView(id: nil, highImage: highImage, lowImage: lowImage)
+    answer.append(foo)
+  }
+  return answer
 }
 
